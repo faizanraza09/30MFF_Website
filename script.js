c781ceda1bca6e61be8cfb64018c5eb7320a8426
Faizan Raza <faizan@Faizans-MacBook-Air.local>
2024-02-12 14:06:38 +0400

    Seperated styling and script, added more styles to the video

---
+++ script.js
@@ -0,0 +1,44 @@
+// Change style of navbar on scroll
+window.onscroll = function () { myFunction() };
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+    }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    // Get form data
+    var formData = new FormData(this);
+
+    // Send form data to Formspree endpoint
+    fetch('https://formspree.io/f/mjvnwveo', {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        document.getElementById("contactForm").reset(); // Reset form fields
+        document.getElementById("successMessage").style.display = "block"; // Show success message
+    })
+    .catch(error => console.error('There was an error!', error));
+});
